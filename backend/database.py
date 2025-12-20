3e17ddc24829378f305e7ef87d260c4b15538fbf
efty <efty3222@gmail.com>
2025-12-21 00:58:08 +0600

    feat: Remove authentication and configuration files to streamline backend structure

--- backend/database.py
+++
@@ -1,6 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from config import settings
-
-client = AsyncIOMotorClient(settings.mongodb_url)
-database = client[settings.database_name]
-users_collection = database.get_collection("users")
